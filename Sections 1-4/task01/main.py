96dac8a92f7d4d8038436297b21cb0916cd47af5
Matheus Carvalho <matheusc200@gmail.com>
2023-11-23 17:03:53 -0300

    ex 1

---
+++ Sections 1-4/task01/main.py
@@ -0,0 +1,13 @@
+# Simples exercise to print the info
+
+name = 'Matt'
+last_name = 'Torres'
+age = 24
+born_year = 1999
+high_metros = 1.69
+
+print("Name:", name)
+print("Age:", age)
+print("Born Year:", born_year)
+print("Adult?", age >= 18)
+print("High:", high_metros, end="m")
